7b6455f789aa2b8eda30ad64a1f194f29f8842e4
yuchengzhangjenny <58120560+jane-yucheng@users.noreply.github.com>
2025-05-30 16:08:23 -0500

    Add minimal vercel.json for proper Next.js deployment configuration

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,16 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-20a8446f813eece4.js","static/chunks/4bd1b696-47484ac7c0434a9c.js","static/chunks/684-f9089df39ad03af7.js","static/chunks/main-app-2c500fb96e69f3a2.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-20a8446f813eece4.js","static/chunks/framework-2475e5daa0674b1a.js","static/chunks/main-7917e0a2f32e6028.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-20a8446f813eece4.js","static/chunks/framework-2475e5daa0674b1a.js","static/chunks/main-7917e0a2f32e6028.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
